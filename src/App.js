22819bfbd93c3bd423aa9274e73a79e67888d356
huy36 <huy36@gmail.com>
2023-07-12 19:23:10 +0700

    day2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { ToastContainer, toast } from 'react-toastify'
+import { Home, Login, Public } from './containers/public/'
+import 'react-toastify/dist/ReactToastify.css'
+// import appReducer from './store/reducer/appReducer'
+import { Routes, Route } from 'react-router-dom'
+import path from './utils/path'
+
 function App() {
-  return <div className='App'>app</div>
+  return (
+    <>
+      <div className=''>
+        <Routes>
+          <Route path={path.PUBLIC} element={<Public />}>
+            <Route path={path.HOME} element={<Home />} />
+            <Route path={path.LOGIN} element={<Login />} />
+          </Route>
+        </Routes>
+      </div>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+      {/* Same as */}
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App
